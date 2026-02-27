3ec2074b1c6b5a9a5c29f6fb2b1030aecca0b151
Andrii Rodzyk <rodzyk@programmer.net>
2026-02-27 19:13:47 +0200

    chore: update canvas dependency to version 3.2.1 and add pixiv logo SVG asset

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   dev: process.env.NODE_ENV === 'dev',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3004,
   host: 'http://localhost',
   cacheCleanupInterval: 1000 * 60 * 60
 };
